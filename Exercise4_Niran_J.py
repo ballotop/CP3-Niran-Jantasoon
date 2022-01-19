ddc0bf21f23f73b3620e6092a374bc69264d8553
Niran <ball_otop@hotmail.com>
2022-01-20 00:19:54 +0700

    Test

---
+++ Exercise4_Niran_J.py
@@ -0,0 +1,10 @@
+score_fe = 81
+score_gb = 82
+score_ics = 83
+score_cp = 84
+
+print("--- Your Score --- ")
+print("Foundation English :", score_fe)
+print("General Business :",score_gb)
+print("Introduction to Computer Systems :", score_ics)
+print("Computer Programming :", score_cp)
